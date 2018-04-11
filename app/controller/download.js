10ea8f5b83d47371270a89aab19d44a1b5686108
kevinbolton729 <kbolton@sohu.com>
2018-04-12 00:46:24 +0800

    edit
    edit

--- app/controller/download.js
+++ app/controller/download.js
@@ -2,7 +2,7 @@
  * @Author: Kevin Bolton
  * @Date: 2018-04-11 22:24:58
  * @Last Modified by: Kevin Bolton
- * @Last Modified time: 2018-04-12 00:43:01
+ * @Last Modified time: 2018-04-12 00:45:27
  */
 'use strict';
 const Controller = require('egg').Controller;
@@ -18,20 +18,12 @@ class DownloadController extends Controller {
     // const { download } = service;
 
     // const fileName = await download.downloadFileModel();
-    const fileName = '1523455087238.txt';
+    const fileName = '1523455087238.jpeg';
 
     const filePath = `${UPLOADURLPREFIX}/public/uploads/images/portrait/${fileName}`;
-    ctx.attachment('adfsdfasdfs.txt');
+    ctx.attachment('adfsdfasdfs.jpeg');
     ctx.set('Content-Type', 'application/octet-stream');
     ctx.body = await fs.createReadStream(filePath);
-    // Set Content-Disposition to "attachment" to signal the client to prompt for download.
-    // Optionally specify the filename of the download.
-    // 设置实体头（表示消息体的附加信息的头字段）,提示浏览器以文件下载的方式打开
-    // 也可以直接设置 ctx.set("Content-disposition", "attachment; filename=" + fileName);
-    // ctx.attachment(fileName);
-    // await send(ctx, fileName, {
-    //   root: `${UPLOADURLPREFIX}/public/uploads/images/adver`,
-    // });
   }
 }
 
