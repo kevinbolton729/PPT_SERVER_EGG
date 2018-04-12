adc146ea2115651fbe4def9eae40cd520819fd61
kevinbolton729 <kbolton@sohu.com>
2018-04-12 23:38:53 +0800

    edit
    edit

--- app/controller/download.js
+++ app/controller/download.js
@@ -2,7 +2,7 @@
  * @Author: Kevin Bolton
  * @Date: 2018-04-11 22:24:58
  * @Last Modified by: Kevin Bolton
- * @Last Modified time: 2018-04-12 00:45:27
+ * @Last Modified time: 2018-04-12 23:23:36
  */
 'use strict';
 const Controller = require('egg').Controller;
@@ -16,14 +16,16 @@ class DownloadController extends Controller {
   async downloadFile() {
     const { ctx } = this;
     // const { download } = service;
+    // console.log(ctx.params.id, 'id');
 
     // const fileName = await download.downloadFileModel();
-    const fileName = '1523455087238.jpeg';
+    const file = '1523455087238.jpeg';
+    const fileExt = file.split('.')[1];
 
-    const filePath = `${UPLOADURLPREFIX}/public/uploads/images/portrait/${fileName}`;
-    ctx.attachment('adfsdfasdfs.jpeg');
+    const filePath = `${UPLOADURLPREFIX}/public/uploads/images/portrait/${file}`;
+    ctx.attachment(`adfsdfasdfs.${fileExt}`);
     ctx.set('Content-Type', 'application/octet-stream');
-    ctx.body = await fs.createReadStream(filePath);
+    ctx.body = await fs.readFileSync(filePath);
   }
 }
 
