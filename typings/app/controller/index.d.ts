d4fbe2db25425daf52a2cd527cbf9922c7ac137c
kevinbolton729 <kbolton@sohu.com>
2018-04-17 21:04:43 +0800

    edit
    edit

--- typings/app/controller/index.d.ts
+++ typings/app/controller/index.d.ts
@@ -1,20 +1,20 @@
 // This file was auto created by egg-ts-helper
 // Do not modify this file!!!!!!!!!
 
-import Adver from "../../../app/controller/adver";
-import Apply from "../../../app/controller/apply";
-import Article from "../../../app/controller/article";
-import Brand from "../../../app/controller/brand";
-import Channel from "../../../app/controller/channel";
-import Download from "../../../app/controller/download";
-import Home from "../../../app/controller/home";
-import Shop from "../../../app/controller/shop";
-import Silde from "../../../app/controller/silde";
-import Site from "../../../app/controller/site";
-import Upload from "../../../app/controller/upload";
-import User from "../../../app/controller/user";
+import Adver from '../../../app/controller/adver';
+import Apply from '../../../app/controller/apply';
+import Article from '../../../app/controller/article';
+import Brand from '../../../app/controller/brand';
+import Channel from '../../../app/controller/channel';
+import Download from '../../../app/controller/download';
+import Home from '../../../app/controller/home';
+import Shop from '../../../app/controller/shop';
+import Silde from '../../../app/controller/silde';
+import Site from '../../../app/controller/site';
+import Upload from '../../../app/controller/upload';
+import User from '../../../app/controller/user';
 
-declare module "egg" {
+declare module 'egg' {
   interface IController {
     adver: Adver;
     apply: Apply;
